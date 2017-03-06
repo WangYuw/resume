68d71bfbde5557d03dcef7c8c5fc8e6e254b64cc
wang_yuw <yuwei.wang.pluie@gmail.com>
2017-03-06 23:41:45 +0100

    header

--- js/langueConfig.js
+++ js/langueConfig.js
@@ -5,5 +5,6 @@ var langues={
 	"skills":["Compétences","Skills","个人技能"],
 	"activities":["Activités","Activities","社会活动经历"],
 	"hobbies":["Intérêts","Hobbies","兴趣爱好"],
-	"nom":["Yuwei WANG","WANG Yuwei","王雨薇"],
+	"nom":["WANG Yuwei","WANG Yuwei","王 雨 薇"],
+	"nom2":["WANG Yuwei","WANG Yuwei","王 雨 薇"],
 }
